db37e23c7e0bece93d32ab51a7b248f71dc6f006
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-25 12:03:54 -0300

    Se agrega la funcionalidad signOut

--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -11,11 +11,12 @@ class UserBloc implements Bloc{
    Stream<FirebaseUser> streamFirebase = FirebaseAuth.instance.onAuthStateChanged;
    Stream<FirebaseUser> get authStatus => streamFirebase;
 
-  //Casos de uso del objeto User:
+  // Casos de uso del objeto User:
   // 1. Sign In a la aplicación:
-  Future<FirebaseUser> signIn(){
-    return _auth_repository.signInFirebase();
-  }
+  Future<FirebaseUser> signIn() => _auth_repository.signInFirebase();
+
+  // Otro caso de uso es LogOut:
+  signOut() => _auth_repository.signOut();
 
   @override
   void dispose() {
