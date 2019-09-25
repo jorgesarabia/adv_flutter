3f6b512969e376662d2057fc7c9c5c2c81c4f838
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-25 10:36:59 -0300

    Los bloc se conectan con la UI, se separa la api que llama a firebase de el repository que es quien usa esos datos, luego en el bloc se instancia al repository

--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/repository/auth_repository.dart';
 
 class UserBloc implements Bloc{
+  final _auth_repository = AuthRepository();
+
+  //Casos de uso del objeto User:
+  // 1. Sign In a la aplicación:
+  Future<FirebaseUser> signIn(){
+    return _auth_repository.signInFirebase();
+  }
+
   @override
   void dispose() {
   }
--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'package:platzi_trips_app/widgets/button_green.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
 
@@ -8,8 +11,13 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+
+  UserBloc userBloc;
+
   @override
   Widget build(BuildContext context) {
+    // Se declara desde el provider, ya que es un Singleton:
+    userBloc = BlocProvider.of(context); 
     return signInGoogleUI();
   }
 
@@ -32,7 +40,9 @@ class _SignInScreenState extends State<SignInScreen> {
                 ),
               ),
               ButtonGreen(
-                onPressed: () => {},
+                onPressed: () {
+                  userBloc.signIn().then((FirebaseUser user)=>print("El ususario es ${user.displayName}"));
+                },
                 title: "Login With Gmail",
                 height: 50.0,
                 width: 300.0,
