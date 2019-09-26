b6fe36b232b5d3e3ab25921c2edfa14c0f7575e6
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-26 13:52:07 -0300

    En el archivo profile_header se agrega el StreamBuilder, para monitorear los datos desde Google

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'package:platzi_trips_app/User/ui/widgets/user_info.dart';
 import 'package:platzi_trips_app/User/ui/widgets/button_bar.dart';
 
 class ProfileHeader extends StatelessWidget {
+  UserBloc userBloc;
+
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of<UserBloc>(context);
 
-    final title = Text(
+    return StreamBuilder(
+      stream: userBloc.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        // Vamos a monitorear el objeto snapshot:
+        switch(snapshot.connectionState){
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+            break;
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+            break;
+          case ConnectionState.active:
+            return null;
+            break;
+          case ConnectionState.done:
+            return null;
+            break;
+        }
+      },
+    );
+
+    /*final title = Text(
       'Profile',
       style: TextStyle(
           fontFamily: 'Lato',
@@ -33,7 +59,15 @@ class ProfileHeader extends StatelessWidget {
           ButtonsBar()
         ],
       ),
-    );
+    );*/
+  }
+
+  Widget showProfileData(AsyncSnapshot snapshot){
+    if(!snapshot.hasData || snapshot.hasError){
+      print("No logueado");
+    } else {
+      print("Logueado");
+    }
   }
 
 }
