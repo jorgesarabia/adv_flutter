439bb4a3580fa11231176d15c1bfa5a3ac6d4f9a
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-28 15:19:57 -0300

    Se crea el gradiente generico

--- lib/widgets/gradient_back.dart
+++ lib/widgets/gradient_back.dart
@@ -6,11 +6,23 @@ class GradientBack extends StatelessWidget {
   String title = "Popular";
   double height = 0.0;
 
-  GradientBack(this.title, this.height);
+  GradientBack({
+    Key key,
+    this.height,// if height = null => fullscreen
+  });
 
   @override
   Widget build(BuildContext context) {
+
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    if(height == null){
+      height = screenHeight;
+    }
+
     return Container(
+      width: screenWidth,
       height: height,
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -25,18 +37,30 @@ class GradientBack extends StatelessWidget {
         )
       ),
 
-
-      child: Text(
-        title,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 30.0,
-          fontFamily: "Lato",
-          fontWeight: FontWeight.bold
+      child: FittedBox(
+        fit: BoxFit.none,
+        alignment: Alignment(-1.5,-0.8),
+        child: Container(
+          width: screenHeight,
+          height: screenHeight,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.05),
+            borderRadius: BorderRadius.circular(screenHeight/2),
+          ),
         ),
       ),
 
-      alignment: Alignment(-0.9, -0.6),
+      // child: Text(
+      //   title,
+      //   style: TextStyle(
+      //     color: Colors.white,
+      //     fontSize: 30.0,
+      //     fontFamily: "Lato",
+      //     fontWeight: FontWeight.bold
+      //   ),
+      // ),
+
+      // alignment: Alignment(-0.9, -0.6),
 
     );
   }
