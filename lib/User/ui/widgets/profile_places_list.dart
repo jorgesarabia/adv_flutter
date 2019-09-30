7d41ac103d02df29111c8e50f3ee3209e44b15cd
ingjorgesarabia <ingjorgesarabia@gmail.com>
2019-09-30 07:16:57 -0300

    Se pone un StreamBuilder

--- lib/User/ui/widgets/profile_places_list.dart
+++ lib/User/ui/widgets/profile_places_list.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_app/Place/model/place.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'profile_place.dart';
 
 class ProfilePlacesList extends StatelessWidget {
+  UserBloc userBloc;
+
   Place place = Place(
       name: 'Knuckles Mountains Range',
       description: 'Hiking. Water fall hunting. Natural bath',
@@ -19,14 +23,42 @@ class ProfilePlacesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of(context);
     return Container(
       margin: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0, bottom: 10.0),
+      child: StreamBuilder(
+        stream: userBloc.streamFirebase,
+        builder: (context, AsyncSnapshot snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return CircularProgressIndicator();
+              break;
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              return Column(
+                children: <Widget>[
+                  ProfilePlace(place),
+                  ProfilePlace(place2),
+                ],
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+
+  /**
+   * 
       child: Column(
         children: <Widget>[
           ProfilePlace(place),
           ProfilePlace(place2),
         ],
       ),
-    );
-  }
+   */
+
 }
